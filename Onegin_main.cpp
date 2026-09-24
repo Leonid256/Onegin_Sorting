c744d7b369c013a5e0819cc1af1ae225e6f51e60
Leonid256 <jb58skw7vn@privaterelay.appleid.com>
2026-09-24 23:04:29 +0300

    FINAL_VERSION
    Была добавлена структура в массив, которая включает указатель на строку и ее длину. Это помогает избежать лишних вызовов strlen
    Также улучшены названия функций, добавлены дополнительные проверки

--- Onegin_main.cpp
+++ Onegin_main.cpp
@@ -5,82 +5,95 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <sys/stat.h>
-//-------------------------------------------------------------------------------------------------
-int ReadFromFile(const char* , char* , struct stat file_info);
-int Add_to_Index(const char** index, const char* big_buffer, size_t count);
-int WriteToFile(const char* name, const char** index, size_t count, const char* mode);
-int WriteSpaces(const char* name, size_t val);
-char *my_strdup(const char *s);
-int Compare_Straight(const void * ptr_a, const void * ptr_b);
-int Compare_Reverse(const void * ptr_a, const void * ptr_b);
-int Compare_to_Initial(const void * ptr_a, const void * ptr_b);
-int QuickSort(void* list, size_t num, size_t size, int (*compare)(const void *, const void *));
-int ChangeValues(char* data, size_t value1, size_t value2, size_t size);
-char *my_strrev(char *str);
-void toxic_free(char** buffer);
 
 //-------------------------------------------------------------------------------------------------
-//const size_t MAX_LINES    = 10000;
-const size_t   BUFFER_SIZE  = 1000;
-const size_t   SPACE        = 10;
-const char     FILE_IN[]    = "/Users/leonid/Documents/GitHub/mipt_course/Onegin/Onegin.txt";
-const char     FILE_OUT[]   = "/Users/leonid/Documents/GitHub/mipt_course/Onegin/Res_Onegin.txt";
-const int      POISON_VALUE = 13;
+struct string_index 
+{
+    char* string;
+    size_t len;
+};
+
+//-------------------------------------------------------------------------------------------------
+int read_file(const char* name, char* big_buffer, size_t count);
+size_t add_to_index(string_index* index, char* big_buffer, size_t count);
+int write_to_file(const char* name, string_index* index, size_t count, const char* mode);
+int write_spaces(const char* name, size_t val);
+char *my_strdup(const char *s);
+int compare_straight(const void * ptr_a, const void * ptr_b);
+int compare_reverse(const void * ptr_a, const void * ptr_b);
+int compare_to_initial(const void * ptr_a, const void * ptr_b);
+int quick_sort(void* list, size_t num, size_t size, int (*compare)(const void *, const void *));
+int change_values(char* data, size_t value1, size_t value2, size_t size);
+char *my_strrev(char *str);
+void toxic_free(void* buffer);
+size_t len_of_file(const char* name);
+
+//-------------------------------------------------------------------------------------------------
+const size_t   BUFFER_SIZE    = 1000;
+const size_t   SPACE          = 10;
+const char     FILE_IN[]      = "Onegin/Onegin.txt";
+const char     FILE_OUT[]     = "Onegin/Res_Onegin.txt";
+const int      POISON_VALUE   = 13;
+const char     RES_DIVISION[] = "---------------------------------------------------------------------------------------------------------------------------------------";
 
 //-------------------------------------------------------------------------------------------------
 int main()
 {
-    struct stat file_info;
-    stat(FILE_IN, &file_info);
+    size_t file_len = len_of_file(FILE_IN);
+    size_t count = file_len / sizeof(char);
 
-    size_t count = file_info.st_size / sizeof(char);
-
-    char* big_buffer = (char*) calloc(file_info.st_size / sizeof(char) + 1, sizeof(char));  //buffer for fread
-    size_t strings = ReadFromFile(FILE_IN, big_buffer, file_info);
-    //printf("%zu\n", count);
-
-    char** index = (char**)calloc(strings, sizeof(char*));
-    count = Add_to_Index((const char**)index, (const char*)big_buffer, count);
-
-    //strait sorting//
-    qsort(index, count, sizeof(char*), Compare_Straight);
-    WriteToFile(FILE_OUT, (const char**)index, count, "w");
-    
-    WriteSpaces(FILE_OUT, SPACE);
-    
-    
-    //reverse sorting//
-    QuickSort(index, count, sizeof(char*), Compare_Reverse);
-    WriteToFile(FILE_OUT, (const char**)index, count, "a");
-
-    WriteSpaces(FILE_OUT, SPACE);
-
-    //initial text//
-    qsort(index, count, sizeof(char*), Compare_to_Initial);
-    WriteToFile(FILE_OUT, (const char**)index, count, "a");
-
-    //free buffer//
-    for (size_t i = 0; i < count; i++)
+    //buffer for fread//
+    char* big_buffer = (char*) calloc(count + 1, sizeof(char));
+    if (big_buffer == NULL)
+        return 1;
+    size_t strings = read_file(FILE_IN, big_buffer, count);
+    if (strings <= 0)
     {
-        index[i] = NULL;
-        toxic_free(&(index[i]));
+        toxic_free(big_buffer);
+        return 1;
     }
 
-    toxic_free(&big_buffer);
+    string_index* index = (string_index*)calloc(strings, sizeof(string_index));
+    if (index == NULL)
+    {
+        toxic_free(big_buffer);
+        return 1;
+    }
+    count = add_to_index((string_index*)index, (char*)big_buffer, count);
+
+    //strait sorting//
+    qsort(index, count, sizeof(string_index), compare_straight);
+    write_to_file(FILE_OUT, (string_index*)index, count, "w");
+    
+    write_spaces(FILE_OUT, SPACE);
+    
+    //reverse sorting//
+    quick_sort(index, count, sizeof(string_index), compare_reverse);
+    write_to_file(FILE_OUT, (string_index*)index, count, "a");
+
+    write_spaces(FILE_OUT, SPACE);
+
+    //initial text//
+    qsort(index, count, sizeof(string_index), compare_to_initial);
+    write_to_file(FILE_OUT, (string_index*)index, count, "a");
+
+    //free buffer//
+    toxic_free(index);
+    toxic_free(big_buffer);
 
     return 0;
 }
 //-------------------------------------------------------------------------------------------------
-int Compare_Straight(const void * ptr_a, const void * ptr_b)
+int compare_straight(const void * ptr_a, const void * ptr_b)
 {
-    const char* pt_a = *(const char**)ptr_a;
-    const char* pt_b = *(const char**)ptr_b;
+    const char* pt_a = ((const string_index*)ptr_a) -> string;
+    const char* pt_b = ((const string_index*)ptr_b) -> string;
 
     while (*pt_a && *pt_b)
     {
-        while (!isalpha(*pt_a))
+        while (*pt_a && !isalpha(*pt_a))
             pt_a++;
-        while (!isalpha(*pt_b))
+        while (*pt_b && !isalpha(*pt_b))
             pt_b++;
 
         char a = tolower((char) *pt_a);
@@ -96,29 +109,29 @@ int Compare_Straight(const void * ptr_a, const void * ptr_b)
     return (tolower((char) *pt_a) - tolower((char) *pt_b));
 }
 //-------------------------------------------------------------------------------------------------
-int Compare_Reverse(const void * ptr_a, const void * ptr_b)
+int compare_reverse(const void * ptr_a, const void * ptr_b)
 {
-    const char* start_a = *(const char**)(ptr_a);
-    const char* start_b = *(const char**)(ptr_b);
+    if (ptr_a == NULL || ptr_b == NULL)
+        return (ptr_a != NULL) - (ptr_b != NULL);
 
-    if (*start_a == '\0' || *start_b == '\0')
-        return (*start_a != '\0') - (*start_b != '\0');
+    const string_index* str_a = (const string_index*)ptr_a;
+    const string_index* str_b = (const string_index*)ptr_b;
 
-    size_t i_a = strlen(start_a) - 1;
-    size_t i_b = strlen(start_b) - 1;
+    ssize_t i_a = (ssize_t)str_a -> len - 1;
+    ssize_t i_b = (ssize_t)str_b -> len - 1;
 
     while (i_a >= 0 && i_b >= 0)
     {
-        while (i_a >= 0 && !isalpha(start_a[i_a]))
+        while (i_a >= 0 && !isalpha((unsigned char)str_a -> string[i_a]))
             i_a--;
-        while (i_b >= 0 && !isalpha(start_b[i_b]))
+        while (i_b >= 0 && !isalpha((unsigned char)str_b -> string[i_b]))
             i_b--;
 
         if (i_a < 0 || i_b < 0)
             break;
 
-        char a = tolower((char) start_a[i_a]);
-        char b = tolower((char) start_b[i_b]);
+        char a = (char)tolower((unsigned char) str_a -> string[i_a]);
+        char b = (char)tolower((unsigned char) str_b -> string[i_b]);
 
         if (a != b)
             return (a - b);
@@ -127,29 +140,43 @@ int Compare_Reverse(const void * ptr_a, const void * ptr_b)
         i_b--;
     }
 
-    return i_a - i_b;
+    if (i_a >= 0 && i_b < 0)
+        return 1;
+    else if (i_a < 0 && i_b >= 0)
+        return -1;
+
+    if (str_a -> len > str_b -> len)
+        return 1;
+    else if (str_a -> len < str_b -> len)
+        return -1;
+
+    return 0;
 }
 //-------------------------------------------------------------------------------------------------
-int Compare_to_Initial(const void * ptr_a, const void * ptr_b)
+int compare_to_initial(const void * ptr_a, const void * ptr_b)
 {
-    if (*(const char*)ptr_a > *(const char*)ptr_b)
+    const string_index* pt_a = (const string_index*)ptr_a;
+    const string_index* pt_b = (const string_index*)ptr_b;
+
+    char* str_a = pt_a -> string;
+    char* str_b = pt_b -> string;
+
+    if (str_a > str_b)
         return 1;
-    if (*(const char*)ptr_a < *(const char*)ptr_b)
+    if (str_a < str_b)
         return -1;
     return 0;
 }
 //-------------------------------------------------------------------------------------------------
-int ReadFromFile(const char* name, char* big_buffer, struct stat file_info)
+int read_file(const char* name, char* big_buffer, size_t count)
 {
     FILE* file = fopen (name, "r");
-    size_t strings = 0;
-
     if (file == NULL)
         return -1;
+    
+    size_t strings = 0;
+    size_t file_len = fread((void*) big_buffer, sizeof(char), count, file);
 
-    size_t file_len = fread((void*) big_buffer, sizeof(char), file_info.st_size / sizeof(char), file);
-
-    //printf("buffer = <%s>\n", big_buffer);
     fclose(file);
 
     char* buffer = (char*) big_buffer;
@@ -165,41 +192,46 @@ int ReadFromFile(const char* name, char* big_buffer, struct stat file_info)
     return strings;
 }
 //-------------------------------------------------------------------------------------------------
-int Add_to_Index(const char** index, const char* big_buffer, size_t count)
+size_t add_to_index(string_index* index, char* big_buffer, size_t count)
 {
-    if (index == NULL || big_buffer == NULL)
+    if (index == NULL || big_buffer == NULL || count == 0)
         return -1;
 
     size_t i_buf = 0, i_ind = 0;
 
-    index[i_ind] = my_strdup(&big_buffer[i_buf]);
-
-    for (i_buf = 1, i_ind = 1; i_buf < count; i_buf++)
+    while (i_buf < count)
     {
-        if (big_buffer[i_buf] == '\0')
-        {
-            if (big_buffer[i_buf + 1] == '\0')
-                continue;
-            else
-                index[i_ind++] = my_strdup(&big_buffer[i_buf + 1]);
-        }
-    }
+        while (i_buf < count && big_buffer[i_buf] == '\0')
+            i_buf++;
 
+        if (i_buf >= count)
+            break;
+
+        index[i_ind].string = &big_buffer[i_buf];
+        size_t start_len = i_buf;
+
+        while (i_buf < count && big_buffer[i_buf] != '\0')
+            i_buf++;
+
+        index[i_ind].len = i_buf - start_len;
+        i_ind++;
+    }
+    
     return i_ind;
 }
 
 //-------------------------------------------------------------------------------------------------
-int WriteToFile(const char* name, const char** index, size_t count, const char* mode)
+int write_to_file(const char* name, string_index* index, size_t count, const char* mode)
 {
     FILE* file = fopen (name, mode);
-    size_t i = 0;
-
     if (file == NULL)
         return -1;
+    
+    size_t i = 0;
 
     while (i < count)
     {
-        fputs((index[i++]), file);
+        fputs((index[i++].string), file);
         fprintf(file, "\n");
     }
 
@@ -208,22 +240,21 @@ int WriteToFile(const char* name, const char** index, size_t count, const char* 
     return i + 1;
 }
 //-------------------------------------------------------------------------------------------------
-int WriteSpaces(const char* name, size_t val)
+int write_spaces(const char* name, size_t val)
 {
     FILE* file = fopen(name, "a");
-    size_t i = 0;
-
     if (file == NULL)
         return -1;
+
+    size_t i = 0;
     
-    fprintf(file, "---------------------------------------------------------------------------------------------------------------------------------------\n");
+    fprintf(file, "%s\n", RES_DIVISION);
     while (i < val)
     {
         fputs("\n", file);
         i++;
     }
-    fprintf(file, 
-"---------------------------------------------------------------------------------------------------------------------------------------\n");
+    fprintf(file, "%s\n", RES_DIVISION);
 
     fclose(file);
 
@@ -244,16 +275,16 @@ char *my_strdup(const char *s)
     return ptr;
 }
 //---------------------------------------------------------------------------------
-int QuickSort(void* list, size_t num, size_t size, int (*compare)(const void *, const void *))
+int quick_sort(void* list, size_t num, size_t size, int (*compare)(const void *, const void *))
 {
-    if (num < 1)
+    if (num <= 1)
         return 0;
 
-    char* data = (char*) list;
+    char* data = ((char*) list);
 
     size_t last = 0;
 
-    ChangeValues(data, (last), (num / 2), size);
+    change_values(data, last, (num / 2), size);
 
     for (size_t i = 1; i < num; i++)
     {
@@ -263,19 +294,19 @@ int QuickSort(void* list, size_t num, size_t size, int (*compare)(const void *, 
         if ((*compare)(pt_pivot, pt_i) > 0)
         {
             last++;
-            ChangeValues(data, (last), (i), size);
+            change_values(data, last, i, size);
         }
     }
 
-    ChangeValues(data, (last), (0), size);
+    change_values(data, last, 0, size);
 
-    QuickSort((void*)(data), last, size, compare);
-    QuickSort((void*)(data + (last + 1) * size), num - last - 1, size, compare);
+    quick_sort((void*)(data), last, size, compare);
+    quick_sort((void*)(data + (last + 1) * size), num - last - 1, size, compare);
 
     return 0;
 }
 //---------------------------------------------------------------------------------
-int ChangeValues(char* data, size_t value1, size_t value2, size_t size)
+int change_values(char* data, size_t value1, size_t value2, size_t size)
 {
     if (value1 == value2)
         return 0;
@@ -293,7 +324,7 @@ int ChangeValues(char* data, size_t value1, size_t value2, size_t size)
 char *my_strrev(char *str)
 {
     size_t len = strlen(str) - 1;
-    for (int i = 0; i <= len / 2; i++)
+    for (size_t i = 0; i <= len / 2; i++)
     {
         char t = str[i];
         str[i] = str[len - i];
@@ -303,8 +334,17 @@ char *my_strrev(char *str)
     return &str[0];
 }
 //---------------------------------------------------------------------------------
-void toxic_free(char** buffer)
+void toxic_free(void* in_buffer)
 {
-    free(*buffer);
-    *buffer = (char*) POISON_VALUE;
+    char* buffer = (char*) in_buffer;
+    free(buffer);
+    *buffer = POISON_VALUE;
+}
+//---------------------------------------------------------------------------------
+size_t len_of_file(const char* name)
+{
+    struct stat file_info;
+    stat(name, &file_info);
+
+    return file_info.st_size;
 }
